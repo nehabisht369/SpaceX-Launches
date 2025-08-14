95cf058be57d1565d39093990189b9b5f80791af
Neha Bisht <nehabisht@Nehas-MacBook-Air.local>
2025-08-14 20:28:29 +0530

    ESLint + Prettier configuration added

---
+++  eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    // ... other extends
+    'prettier',
+  ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+// .prettierrc.js
+module.exports = {
+  singleQuote: true,
+  bracketSameLine: true,
+  endOfLine: 'auto',
+  // Add other Prettier options as needed
+};
